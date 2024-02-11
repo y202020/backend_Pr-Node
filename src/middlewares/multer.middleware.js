3c39dc281dfc4136f67a2e4d7efa706a4f19f071
y202020 <yash2910bhardwaj@gmail.com>
2024-02-11 14:51:33 +0530

    middelware for file upload

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+        //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        console.log("clodinary file local storing details:",file)
+      cb(null, file.originalname)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+
+         
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localPath) => {
+    try {
+        if (!localPath) return null;
+        //upload file
+        const response = await cloudinary.uploader.upload(localPath, {
+            resource_type:"auto"
+        })
+        //file uploaded
+        console.log("file uploaded on cloudinary:", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localPath) //remove the locally saved temp file as failed uploadation.
+    }
+       
+}
+
+
+export {uploadOnCloudinary}
+  
+
