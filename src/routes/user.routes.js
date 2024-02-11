bf719a53568510ca711bd83f304b9f9f15b4ced1
y202020 <yash2910bhardwaj@gmail.com>
2024-02-12 02:01:02 +0530

    added new router for ddifferent controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
-import { loginUser, registerUser,logOutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {
+    loginUser,
+    registerUser,
+    logOutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateAvatar,
+    updateCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} from "../controllers/user.controller.js";
+
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -24,6 +37,17 @@ router.route("/login").post(loginUser)
 //secured route
 router.route("/logout").post(verifyJwt, logOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJwt, changeCurrentPassword)
+router.route("/current-user").get(verifyJwt, getCurrentUser)
+router.route("/update-account").patch(verifyJwt, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJwt, upload.single("avatar"),updateAvatar)
+router.route("/cover-Image").patch(verifyJwt, upload.single("coverImage"), updateCoverImage)
+
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile)
+
+router.route("/history").get(verifyJwt, getWatchHistory)
+
 
 // router.route("/login").post(registerUser)
 
